6c043bae86f3aaebdc4f2e302e2ec3992a61cea8
jmllubes <jmllubes@gmail.com>
2026-04-14 15:00:01 +0200

    insert

--- crud/insert.vb
+++ crud/insert.vb
@@ -1,4 +1,11 @@
-﻿Public Class insert
+﻿Imports MySql.Data.MySqlClient
+
+Public Class insert
+    Public dbconn As New MySqlConnection
+    Public sql As String
+    Public dbcomm As New MySqlCommand
+    Private dbadapter As MySqlDataAdapter
+
     Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
         Dim isbn As String
         Dim titol As String
@@ -12,14 +19,29 @@
         editorial = TextBox5_editorial.Text
         preu = Decimal.Parse(TextBox6_preu.Text)
         categoria = TextBox3_Categoria.Text
+        dbconn = New MySqlConnection("Data Source=localhost;user id=root;password=;port=3306;database=biblioteca")
+        Try
+            dbconn.Open()
+            sql = "INSERT INTO llibre (isbn, titol, autor, editorial, preu, categoria) VALUES (@isbn, @titol, @autor, @editorial, @preu, @categoria)"
+            dbcomm = New MySqlCommand(sql, dbconn)
+            dbcomm.Parameters.AddWithValue("@isbn", isbn)
+            dbcomm.Parameters.AddWithValue("@titol", titol)
+            dbcomm.Parameters.AddWithValue("@autor", autor)
+            dbcomm.Parameters.AddWithValue("@editorial", editorial)
+            dbcomm.Parameters.AddWithValue("@preu", preu)
+            dbcomm.Parameters.AddWithValue("@categoria", categoria)
+            dbcomm.ExecuteNonQuery()
+            dbconn.Close()
 
-        MsgBox("ISBN: " & isbn & vbCrLf &
-               "Títol: " & titol & vbCrLf &
-               "Autor: " & autor & vbCrLf &
-               "Editorial: " & editorial & vbCrLf &
-               "Preu: " & preu & vbCrLf &
-               "Categoria: " & categoria)
+        Catch ex As Exception
+            MsgBox("Connection Error: " & ex.Message.ToString)
+        End Try
 
 
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Form1.Show()
+        Me.Hide()
     End Sub
 End Class
